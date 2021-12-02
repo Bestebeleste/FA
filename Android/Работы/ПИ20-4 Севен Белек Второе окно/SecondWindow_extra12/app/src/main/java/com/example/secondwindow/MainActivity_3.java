c38602411c387abca49fa99ff7a786452754d825
Belek <belek_seven@mail.ru>
2021-12-02 17:22:15 +0700

    practice Seven

---
+++ Android/Работы/ПИ20-4 Севен Белек Второе окно/SecondWindow_extra12/app/src/main/java/com/example/secondwindow/MainActivity_3.java
@@ -0,0 +1,57 @@
+package com.example.secondwindow;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity_3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_3);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        Intent intent;
+        switch (item.getItemId()) {
+            case R.id.activity_1:
+                intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.activity_2:
+                intent = new Intent(this, MainActivity_2.class);
+                startActivity(intent);
+                break;
+            case R.id.activity_3:
+                intent = new Intent(this, MainActivity_3.class);
+                startActivity(intent);
+                break;
+            case R.id.activity_4:
+                intent = new Intent(this, MainActivity_4.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
